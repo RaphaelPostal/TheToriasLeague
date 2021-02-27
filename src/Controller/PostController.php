9c9f29e962a70a1018e723993b76ce6667183547
Raphaël Postal <raphael.postal@etudiant.univ-reims.fr>
2021-02-27 11:17:21 +0100

    Création des entités pour le jeu et config de création d'une game

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -16,19 +16,27 @@ class PostController extends AbstractController
     /**
      * @Route("/post", name="post")
      */
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntityManagerInterface $entityManager, PostCategoryRepository $postCategoryRepository): Response
+
     {
+
+    //on doit recup l'objet category d'id 1
+        $category = $postCategoryRepository->find(1);
         //Créer une page qui va sauvegarder un post avec le nom Post 1 à la date courante avec comme contenu Lorem ipsum et en enable à true.
         $post = new Post();
         $post->setTitle('PostSuper');
         $post->setDateCreated(new \DateTime());
         $post->setContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec semper tellus vitae ante euismod tempus. Quisque non est luctus, tempus felis quis, pellentesque nulla. Sed hendrerit nulla ut ullamcorper dignissim. Aliquam eleifend, mi a gravida ullamcorper, odio nisl volutpat diam, sit amet commodo purus massa et libero. Nunc in porttitor libero, sit amet elementum ex. Donec nisi tellus, malesuada porta porttitor auctor, consequat eget felis. Nunc rhoncus sapien imperdiet nisi mattis pharetra non sed augue. Curabitur rhoncus libero magna, egestas pretium ipsum dignissim at. Nullam vulputate ut tortor et dignissim. Pellentesque eget diam sed augue auctor mollis quis in turpis. Donec gravida justo at magna dictum, eget posuere nibh ultrices. Suspendisse at aliquam mauris. Nam iaculis ipsum justo, at viverra turpis vulputate id. Sed convallis faucibus mi. Mauris tempor ante a dignissim porta.');
         $post->setEnable(true);
+        $post->setCategory($category);
         $entityManager->persist($post);
         $entityManager->flush();
 
+        $postCategories = $postCategoryRepository->findAll();
+
         return $this->render('post/index.html.twig', [
-            'post' => $post
+            'post' => $post,
+            'postCategories' => $postCategories
         ]);
     }
 
