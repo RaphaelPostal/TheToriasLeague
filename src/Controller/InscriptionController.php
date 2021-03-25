47de7597a5d46aab0107ea01dfc8f80b281b2337
Raphaël Postal <raphael.postal@etudiant.univ-reims.fr>
2021-03-25 16:15:22 +0100

    header provisoire

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -32,29 +32,37 @@ class InscriptionController extends AbstractController
     {
         //test si email dejà pris
         $existe = $userRepository->findOneby(["email"=>$_POST['email']]);
-        if(!$existe){
+        if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+            if(!$existe){
+                $new_user = new User();
+                $new_user->setFirstName($_POST['prenom']);
+                $new_user->setLastName($_POST['nom']);
+                $new_user->setBirthday(new \DateTime($_POST['birthday']));
+                $new_user->setEmail($_POST['email']);
+                $new_user->setPseudo($_POST['pseudo']);
+                $new_user->setPassword(password_hash($_POST['mdp'], PASSWORD_ARGON2I));
+                $new_user->setRoles(["ROLE_JOUEUR"]);
+                $new_user->setElo(1000);
 
-            $new_user = new User();
-            $new_user->setFirstName($_POST['prenom']);
-            $new_user->setLastName($_POST['nom']);
-            $new_user->setBirthday(new \DateTime($_POST['birthday']));
-            $new_user->setEmail($_POST['email']);
-            $new_user->setPseudo($_POST['pseudo']);
-            $new_user->setPassword(password_hash($_POST['mdp'], PASSWORD_ARGON2I));
-            $new_user->setRoles(["ROLE_JOUEUR"]);
-            $new_user->setElo(1000);
-
-            $entityManager->persist($new_user);
-            $entityManager->flush();
-            return $this->render('inscription/valide.html.twig', [
-                'new_user'=>$new_user,
+                $entityManager->persist($new_user);
+                $entityManager->flush();
+                return $this->render('inscription/valide.html.twig', [
+                    'new_user'=>$new_user,
 
 
-            ]);
+                ]);
+            }else{
+                return $this->render('inscription/index.html.twig', [
+                    'erreur'=>'Cet utilisateur est déjà inscrit !'
+
+
+                ]);
+            }
+
         }else{
 
             return $this->render('inscription/index.html.twig', [
-                'erreur'=>'Déjà inscrit !'
+                'erreur'=>'Email invalide !'
 
 
             ]);
