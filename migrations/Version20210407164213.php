9b53238616c1aa02d73fd0105aafab0ee169b056
Raphaël Postal <raphael.postal@etudiant.univ-reims.fr>
2021-04-07 19:31:13 +0200

    fix bug bdd

--- migrations/Version20210407164213.php
+++ migrations/Version20210407164213.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210331163502 extends AbstractMigration
+final class Version20210407164213 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,12 @@ final class Version20210331163502 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD inscription DATE NOT NULL');
+        //$this->addSql('ALTER TABLE round ADD CONSTRAINT FK_C5EEEA34E48FD905 FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `user` DROP inscription');
+        //$this->addSql('ALTER TABLE round DROP FOREIGN KEY FK_C5EEEA34E48FD905');
     }
 }
