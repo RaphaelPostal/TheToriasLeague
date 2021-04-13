b80c5e46442a0594a1256ec1eeee8f42837a1d2f
Raphaël Postal <raphael.postal@etudiant.univ-reims.fr>
2021-04-13 23:31:27 +0200

    premieres stats

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -10,10 +10,14 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin")
+ */
+
 class DashboardController extends AbstractDashboardController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/accueil", name="accueil")
      */
     public function index(): Response
     {
@@ -31,5 +35,14 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-users', User::class);
         yield MenuItem::linkToCrud('Parties', 'fas fa-users', Game::class);
+        yield MenuItem::linkToRoute('Statistiques', 'fa fa-tag', 'back_stats');
+    }
+
+    /**
+     * @Route("/stats", name="back_stats")
+     */
+    public function stats(): Response
+    {
+        return $this->render('stats/index.html.twig');
     }
 }
